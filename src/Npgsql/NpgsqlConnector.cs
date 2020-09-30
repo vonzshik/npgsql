9a3c77a73c9761a14c22fe9f18c6426ce09585cc
Nikita Kazmin <vonzshik@gmail.com>
2020-09-30 11:31:15 +0300

    Fixed cancellation for copy operations

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -1161,7 +1161,8 @@ namespace Npgsql
                             Debug.Assert(msg != null, "Message is null for code: " + messageCode);
                             return msg;
                         }
-                        catch (NpgsqlException e) when (!readingNotifications2 && e.InnerException is TimeoutException)
+                        // Copy operation will be cancelled while closing the connection
+                        catch (NpgsqlException e) when (!readingNotifications2 && CurrentCopyOperation is null && e.InnerException is TimeoutException)
                         {
                             // Cancel request is send, but we were unable to read a response from PG due to timeout
                             if (_originalTimeoutException != null)
